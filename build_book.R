5627a399621a8417d7bea1e3d98c0172e084fb66
Martin Schmettow <schmettow@web.de>
2018-01-10 20:43:18 +0100

    LM: new section on correlations

--- build_book.R
+++ build_book.R
@@ -1,2 +1,2 @@
-bookdown::render_book(input = "_bookdown.yml", new_session = T, output_dir = "Book/HTML")
+bookdown::render_book(input = "_bookdown.yml", new_session = T, output_dir = "Book/HTML", css = "New_Stats.css")
 
