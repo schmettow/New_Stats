dce6910a215abb37d49dbabea9bd646c8ac61f45
Schmettow <m.schmettow@utwente.nl>
2018-10-30 14:11:59 +0100

    all output is going to /docs to prepare for publishing on Github pages

--- build_book.R
+++ build_book.R
@@ -1,2 +1,29 @@
-bookdown::render_book(input = "_site.yml", new_session = T, output_dir = "Book/HTML")
+file.remove("_main.Rmd")
+
+### HTML
+
+bookdown::render_book("_bookdown.yml", 
+                      output_format = "bookdown::gitbook", 
+                      clean = TRUE, 
+                      new_session = T,
+                      output_dir = "docs/", 
+                      encoding = "UTF-8")
+
+### Epub
+
+bookdown::render_book("_bookdown.yml",
+                      output_format = "bookdown::epub_book", 
+                      clean = TRUE, 
+                      new_session = T,
+                      output_dir = "docs/", 
+                      encoding = "UTF-8")
+
+### PDF
+
+bookdown::render_book("_bookdown.yml", 
+                      output_format = "bookdown::pdf_book", 
+                      clean = T,
+                      new_session = T,
+                      output_dir = "docs/",
+                      encoding = "UTF-8")
 
